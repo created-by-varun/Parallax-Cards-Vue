6672e72fd6b451e53e10b4e4a0917ad92fd12952
Varun V <varunvirupaksha321@gmail.com>
2020-05-24 10:43:18 +0530

    updated Product.vue

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="product">
-        product
+        <div class="product-inner">
+            <div class="product-text-wrap">
+                <h2 class="bg-text">
+                    {{product.bgtext}}
+                </h2>
+            </div>
+            <div class="product-image-wrap">
+                <img :src="product.src" class="image" alt="">
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'product',
+    props: ['product']
+}
+</script>
